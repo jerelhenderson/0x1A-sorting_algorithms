f2dc99a1bbac3d4fc5e3c6019a66ca418bef1e26
shannonanderson77 <shannonanderson77@gmail.com>
2020-02-01 10:57:48 +0000

    remove printf statements

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,41 +1,36 @@
 #include "sort.h"
 
 /**
- *
- *
- *
+ * bubble_sort - a function that sorts an array of integers in ascending
+ * order using the Bubble sort algorithm
+ * @array: an array to sort
+ * @size: size of the array
+ * Return: NULL
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i;
 	size_t j;
-	size_t pos;
-	size_t next;
-	size_t swapped;
+	size_t swapped = 0;
 
-	j = 0;
-	swapped = 0;
-
-	while (j < size - 1)
+	/* for looping over array and checking if swap occurred */
+	for (j = 1; j <= size && swapped == 0; j++)
 	{
-		i = 0;
-		while (i < size - 1)
+		swapped = 1;
+		for (i = 1; i < size; i++)
 		{
-			if (array[i] > array[i + 1])
+			/* if array[0] is bigger than array[1] and so on down the line */
+			if (array[i - 1] > array[i])
 			{
-				pos = array[i + 1];
-				next = array[i];
-				array[i] = pos;
-				array[i + 1] = next;
+				/* swap without a temp variable */
+				array[i - 1] = array[i - 1] + array[i];
+				array[i] = array[i - 1] - array[i];
+				array[i - 1] = array[i - 1] - array[i];
+				/* a swap happened so make swapped == 0 */
+				swapped = 0;
 				print_array(array, size);
-				swapped = 1;
 			}
-			i++;
 		}
-		if (swapped == 0)
-			break;
-		swapped = 0;
 	}
-	j++;
 }
 
